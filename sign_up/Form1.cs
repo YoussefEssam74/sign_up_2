7ef8938db491a6102f406256acb7dbbfa9b6686a
workstation <workstation@DESKTOP-5EH2KT7>
2024-05-03 20:09:13 +0300

    Add project files.

---
+++ sign_up/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sign_up
+{
+    public partial class Form1 : Form
+    {
+        public static string Firstname { get; set; }
+        public string user;
+        public string password;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string filename, record;
+        StreamReader sr;
+        FileStream sign_up_file;
+        int yehia7arak;
+        int yehia7arak2;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            filename = @"C:\Users\workstation\OneDrive\Desktop\y^5.txt";
+
+            sign_up_file = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            sr = new StreamReader(sign_up_file);
+            record = sr.ReadLine();
+            string[] check;
+            while (record != null)
+            {
+                check = record.Split(',');
+                if (email_txt.Text == check[2] && pass_txt.Text == check[3])
+                {
+                    MessageBox.Show("found");
+                    return;
+
+                }
+                record = sr.ReadLine();
+               
+                
+            }
+            if(email_txt.Text!="" && pass_txt.Text!="")
+                MessageBox.Show("not found");
+           
+        }
+
+
+        private void signup_btn_Click(object sender, EventArgs e)
+        { 
+            
+            this.Hide();
+            Form5 form5 = new Form5(this);
+            form5.ShowDialog();
+            this.Show();
+        }
+
+        private void email_txt_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+        private void pass_txt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void getdata(string username, string password, string firstname)
+        {
+            user = username;
+            this.password = password;
+            Firstname = firstname;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void email_labl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            pass_txt.UseSystemPasswordChar = !pass_txt.UseSystemPasswordChar;
+            button1.Text = pass_txt.UseSystemPasswordChar ? "Show" : "Hide";
+        }
+
+        private void pass_labl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void email_txt_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ sign_up/Form5.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Windows.Forms.VisualStyles;
+
+namespace sign_up
+{
+    public partial class Form5 : Form
+    {
+       // private Form1 form1Reference;
+        public Form5(Form1 form1)
+        {
+            InitializeComponent();
+           // form1Reference = form1;
+        }
+        FileStream sign_up_file;
+        StreamWriter sw;
+        string filename,record;
+        
+        private void label6_Click(object sender, EventArgs e)
+        {
+            label6.Size = new Size(66, 50);
+        }
+
+        private void save_btn_Click(object sender, EventArgs e)
+        {
+        
+
+            string firstname="", secondname = "", email = "", pass = "", check = "";
+            //save first name
+            firstname = firstname_txt.Text;
+            //save second name
+            secondname = secondname_txt.Text;
+            //save email
+            email = email_txt.Text;
+            //save pass
+            pass = pass_txt.Text;
+            check = conf_txt.Text;
+
+            // check the pass
+            check = conf_txt.Text;
+            if (is_this_email(email)&&(firstname != "" && secondname != "" && email != "" && pass != ""))
+            {
+
+              MessageBox.Show("This Email is Already Exists");
+                email_txt.Text = null;
+                sign_up_file.Close();
+               return;
+
+            }
+            if (firstname != "" && secondname != "" && email != "" && pass != "")
+            {
+                if (pass == check)
+                {
+                    sign_up_file.Seek(0, SeekOrigin.End);
+                    record = firstname + "," + secondname + "," + email + "," + pass;
+                    sw.WriteLine(record);
+                    sw.Flush();
+                    sign_up_file.Close();
+                    this.Close();
+                  
+                }
+                else if (pass != check)
+                {
+                    MessageBox.Show("the Password is wrong");
+                    pass_txt.Text = "";
+                    conf_txt.Text = "";
+                    sign_up_file.Close();
+
+                }
+
+
+            }
+            else 
+            {
+                MessageBox.Show("There are blanks that need to be completed.");
+                sign_up_file.Close();
+
+
+            }
+
+        }
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            filename = @"C:\Users\workstation\OneDrive\Desktop\y^5.txt";
+            sign_up_file = new FileStream(filename, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(sign_up_file);
+        }
+
+        private void conf_txt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool is_this_email(string email)
+        {
+            StreamReader sr = new StreamReader(sign_up_file);
+            sign_up_file.Seek (0, SeekOrigin.Begin);
+            while ( !sr.EndOfStream)
+            {
+                string line = sr.ReadLine();    
+                if(line.StartsWith(email))
+                    {
+                    sw.Close();
+                    return true;
+                }
+            }
+           sw.Close();
+            return false;
+        }
+    }
+
+}
